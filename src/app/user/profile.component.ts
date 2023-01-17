e529012f4b76d1da3299ac1f097b062399b64bf5
Kesja <joannakatarzynabach@gmail.com>
2023-01-17 15:29:42 +0100

    add multiple validators

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -44,7 +44,7 @@ export class ProfileComponent implements OnInit {
   ngOnInit(): void {
     this.firstName = new FormControl(
       this.authService.currentUser.firstName,
-      Validators.required
+      [Validators.required, Validators.pattern('[a-zA-Z].*')]
     );
 
     this.lastName = new FormControl(
